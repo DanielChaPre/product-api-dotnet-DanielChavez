9bcf1b3d10c19ee583596e3ca746f4dee2d0c147
Daniel Chavez <danchavez_hp@hotmail.com>
2025-05-28 03:03:10 -0600

    Creación de un modelo o clase que servira  para el retorno de la información, tanto mensajes, estatus y el data necesario en caso de consulta

---
+++ product-api-dotnet-DanielChavez/DTOs/RespuestaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace product_api_dotnet_DanielChavez.DTOs
+{
+    public class RespuestaDTO
+    {
+        public string Codigo { get; set; }
+        public string Mensaje { get; set; }
+        public object? Data  { get; set; }
+    }
+}
