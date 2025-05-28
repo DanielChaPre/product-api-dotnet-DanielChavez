02d85ad3b47a8e5e05347297ec8a88b8eabfe29d
Daniel Chavez <danchavez_hp@hotmail.com>
2025-05-28 03:20:26 -0600

    En el archivo data se agregaron 2 metodos de busqueda uno general que traera todos los productos y otro por id de un producto. En el Controller se agrego los metodos GET de http, para su invocaci[on y hacer uso de los metodos del data.

--- product-api-dotnet-DanielChavez/Controllers/ProductController.cs
+++ product-api-dotnet-DanielChavez/Controllers/ProductController.cs
@@ -15,5 +15,55 @@ namespace product_api_dotnet_DanielChavez.Controllers
             // Inicializamos la clase de acceso a datos
             _data = new ProductData();
         }
+
+        /// <summary>
+        ///     Este metodo nos permite obtener todos los productos de la base de datos
+        /// </summary>
+        /// <returns>
+        ///     El valor de retorno es un objeto de tipo IActionResult que mostraria el estatus de la respuesta HTTP.
+        /// </returns>
+        [HttpGet]
+        [Route("products")]
+        public IActionResult GetProducts()
+        {
+            // Llamamos al método de acceso a datos para obtener los productos
+            var respuesta = _data.GetProducts();
+            if (respuesta.Codigo == "1")
+            {
+                return Ok(respuesta);
+            }
+            else
+            {
+                return BadRequest(respuesta);
+            }
+        }
+
+
+        /// <summary>
+        ///     Este metodo nos permite obtener un producto por su id
+        /// </summary>
+        /// <param name="id"> Este parametro contiene el id del producto a buscar, este valor es ingresado por el usuario </param>
+        /// <returns>
+        ///     El valor de retorno es un objeto de tipo IActionResult que mostraria el estatus de la respuesta HTTP.
+        /// </returns>
+        [HttpGet]
+        [Route("product/{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            // Llamamos al método de acceso a datos para obtener un producto por su ID
+            var respuesta = _data.GetProductById(id);
+            if (respuesta.Codigo == "1")
+            {
+                return Ok(respuesta);
+            }
+            else if (respuesta.Codigo == "0")
+            {
+                return NotFound(respuesta);
+            }
+            else
+            {
+                return BadRequest(respuesta);
+            }
+        }
     }
 }
--- product-api-dotnet-DanielChavez/Data/ProductData.cs
+++ product-api-dotnet-DanielChavez/Data/ProductData.cs
@@ -1,4 +1,6 @@
-﻿using product_api_dotnet_DanielChavez.Models.DB;
+﻿using product_api_dotnet_DanielChavez.DTOs;
+using product_api_dotnet_DanielChavez.Models.DB;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace product_api_dotnet_DanielChavez.Data
 {
@@ -11,7 +13,80 @@ namespace product_api_dotnet_DanielChavez.Data
             //Inicializamos el contexto de la base de datos
             _context = new PruebaApiContext();
         }
+        /// <summary>
+        ///     Este metodo nos permite obtener todos los productos de la base de datos
+        /// </summary>
+        /// <returns>
+        ///      El valor de retorno es un objeto de tipo RespuestaDTO que contiene el codigo, mensaje y los datos de los productos obtenidos.
+        /// </returns>
+        public RespuestaDTO GetProducts()
+        {
+            //Variable de respuesta
+            RespuestaDTO respuesta = new RespuestaDTO();
+            try
+            {
+                //Obtenemos los productos de la base de datos
+                var products = _context.Products.ToList();
+                if (products != null && products.Count > 0)
+                {
+                    respuesta.Codigo = "1";
+                    respuesta.Mensaje = "Productos obtenidos correctamente.";
+                    respuesta.Data = products;
+                }
+                else
+                {
+                    respuesta.Codigo = "0";
+                    respuesta.Mensaje = "No se encontraron productos.";
+                    respuesta.Data = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de error, asignamos el mensaje de error
+                respuesta.Data = null;
+                respuesta.Codigo = "-1";
+                respuesta.Mensaje = "Error al obtener los productos: " + ex.Message;
+            }
+            return respuesta;
+        }
 
+        /// <summary>
+        ///     Este m[etodo nos permite obtener un producto por su id
+        /// </summary>
+        /// <param name="id"> Este parametro seria el id del producto </param>
+        /// <returns> El valor de retorno es un tipo RespuestaDTO que contiene el codigo de estatus de ejecucion, el mensaje del resultado de 
+        ///  la ejecucion y el data o el objeto de retorno buscado por el id.
+        /// </returns>
 
+        public RespuestaDTO GetProductById(int id)
+        {
+            //Variable de respuesta
+            RespuestaDTO respuesta = new RespuestaDTO();
+            try
+            {
+                //Obtenemos el producto por id
+                var product = _context.Products.FirstOrDefault(p => p.Id == id);
+                if (product != null)
+                {
+                    respuesta.Codigo = "1";
+                    respuesta.Mensaje = "Producto obtenido correctamente.";
+                    respuesta.Data = product;
+                }
+                else
+                {
+                    respuesta.Codigo = "0";
+                    respuesta.Mensaje = "Producto no encontrado.";
+                    respuesta.Data = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                //En caso de error, asignamos el mensaje de error
+                respuesta.Data = null;
+                respuesta.Codigo = "-1";
+                respuesta.Mensaje = "Error al obtener el producto: " + ex.Message;
+            }
+            return respuesta;
+        }
     }
 }
